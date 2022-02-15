050474fad41c754fabc8b894e51a7ffb57df4ca6
Mohammad Smadi <mo.smadi@outlook.com>
2022-02-15 21:33:04 +0200

    modify menu

--- public/eventPage.js
+++ public/eventPage.js
@@ -1,11 +1,10 @@
-var contextMenuItem =
-{
-    "id": "annotate",
-    "title": "Click To Add Comment 👆", /* what appears in the menu */
-    "contexts": ['page']  /* to make this appear only when user selects something on page */
-}
-
-chrome.contextMenus.create(contextMenuItem);
+chrome.contextMenus.create(
+    {
+        "id": "annotate",
+        "title": "Click To Add Comment 👆", /* what appears in the menu */
+        "contexts": ['selection']  /* to make this appear only when user selects something on page */
+    }
+);
 
 chrome.contextMenus.onClicked.addListener( (clickData) => {
     if(clickData.menuItemId == "annotate"){
