409c5dd70c1e686077d391bde3e17158afed7af6
Mohammad Smadi <mo.smadi@outlook.com>
2022-04-12 17:12:59 +0300

    modify the event page

--- public/background/eventPage.js
+++ public/background/eventPage.js
@@ -12,7 +12,23 @@ chrome.contextMenus.onClicked.addListener( (clickData,tab) => //
     {
       let ContextCode = "window.getSelection().anchorNode.textContent"
       let LengthCode = "window.getSelection().anchorNode.textContent.length"
-      getContext(clickData,tab,ContextCode,LengthCode)
+
+      let selectedText    =   clickData.selectionText;
+      let pageUrl         =   clickData.pageUrl;
+      let pageTitle       =   tab.title;
+      //let context         =   getContext(ContextCode,LengthCode);
+      //let countInPage     =   getNumberOfWordsSameInPage(selectedText);
+      //let countInContext  =   getNumberOfWordsInTheContest(selectedText,context);
+
+
+      
+      alert("Selection Text : "   + selectedText)
+      alert("Page URL : "            + pageUrl)
+      alert("Page Title : "          + pageTitle)
+      alert("Count In Page : "       + countInPage)
+      alert("Context is : "       + context)
+
+      //getContext(clickData,tab,ContextCode,LengthCode)
     }
 })
 
@@ -37,11 +53,126 @@ function getContext(clickData,tab,ContextCode,LengthCode)
     {
       chrome.tabs.executeScript({code: ContextCode }, (selection) => 
       {
-          alert("Selection Text : " + clickData.selectionText)
-          alert("Page URL" + clickData.pageUrl)
-          alert("Page Title" + tab.title)
+          return selection;
+      });
+    }
+  });
+}
+
+function getNumberOfWordsSameInPage(selectedText)
+{
+  let page = document.body.innerText;
+	let wordInRegex = new RegExp("SPA" , "ig");
+  let count = (page.match(wordInRegex )).length;
+  alert( count);
+}
+
+
+/*
+
+chrome.contextMenus.create(
+    {
+        "id": "annotate",
+        "title": "Click To Add Comment 👆", /* what appears in the menu 
+        "contexts": ['selection']  /* to make this appear only when user selects something on page 
+    }
+);
+
+chrome.contextMenus.onClicked.addListener( (clickData,tab) => //
+{
+    if(clickData.menuItemId == "annotate")
+    {
+      let ContextCode     =   "window.getSelection().anchorNode.textContent"
+      let LengthCode      =   "window.getSelection().anchorNode.textContent.length"
+      //let localNameCode   =   "window.getSelection().anchorNode.parentNode.localName"
+
+      let selectedText    =   clickData.selectionText;
+      //let context         =   getContext(ContextCode,LengthCode);
+      let countInPage     =   getNumberOfWordsSameInPage(selectedText);
+      let countInContext  =   getNumberOfWordsInTheContest(selectedText,context);
+
+
+      let pageUrl         =   clickData.pageUrl;
+      let pageTitle       =   tab.title;
+    
+    
+    
+      alert(pageUrl)
+      alert(pageTitle)
+      alert(countInPage)
+      alert(countInContext)
+    }
+})
+
+function getContext(ContextCode,LengthCode)//,localNameCode
+{
+  // chrome.tabs.executeScript({code: localNameCode }, (localName) =>
+  // {
+  //   if(localName == "p")
+  //   {
+  //     chrome.tabs.executeScript({code: ContextCode }, (selection) => 
+  //     {
+  //        return selection
+  //     });
+  //   }
+
+  //   else
+  //   {
+  //     let parentNode = ".parentNode";
+  //     let indexContext = ContextCode.lastIndexOf(".")
+
+  //     var newContextCode = [ContextCode .slice(0, indexContext), parentNode, ContextCode .slice(indexContext)].join('');
+  //     var newlocalNameCode = [localNameCode .slice(0, indexContext), parentNode, localNameCode .slice(indexContext)].join('');
+
+  //     getContext(newContextCode,newlocalNameCode)
+  //   }
+
+  // })
+  chrome.tabs.executeScript({code: LengthCode }, (length) => 
+  {
+    alert("Context Length is : " + length)
+    if(length<320)
+    {
+      let parentNode = ".parentNode";
+      let indexContext = ContextCode.lastIndexOf(".")
+      var newContextCode = [ContextCode .slice(0, indexContext), parentNode, ContextCode .slice(indexContext)].join('');
+
+      let parentLength = ".parentNode";
+      let indexLength = ContextCode.lastIndexOf(".")
+      var newLengthCode = [LengthCode .slice(0, indexLength), parentLength, LengthCode .slice(indexLength)].join('');
+
+      getContext(newContextCode,newLengthCode)
+    }
+    
+    else
+    {
+      chrome.tabs.executeScript({code: ContextCode }, (selection) => 
+      {
           alert("Context : " + selection)
       });
     }
   });
 }
+
+function getNumberOfWordsSameInPage(selectedText)
+{
+  var page = document.body.textContent;
+	var wordInRegex = new RegExp(selectedText , "ig");
+  return (page.match(wordInRegex )).length;
+}
+
+function getNumberOfWordsInTheContest(selectedText,context)
+{
+	var wordInRegex = new RegExp(selectedText , "ig");
+  return (context.match(wordInRegex )).length;
+}
+
+function getLocalName(localnameCode)
+{
+  chrome.tabs.executeScript({code: localnameCode }, (localName) => 
+  {
+    return localName;
+  })
+}
+
+*/
