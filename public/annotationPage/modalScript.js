f438061f2a620ecf1ac17e96ebfd56e02eee5be3
Mohammad Smadi <mo.smadi@outlook.com>
2022-04-15 02:18:47 +0300

    Modify The Modal Frontend

--- public/annotationPage/modalScript.js
+++ public/annotationPage/modalScript.js
@@ -4,9 +4,9 @@ var selectedText = params.get("selectedText");
 var pageUrl = params.get("pageUrl");
 var pageTitle = params.get("pageTitle")
 
-document.getElementById("demo").innerHTML = "The Selected Word Is : " + selectedText;
-document.getElementById("demo1").innerHTML = "The Page URL Is : " +  pageUrl;
-document.getElementById("demo2").innerHTML = "The Page Title Is: " + pageTitle;
+document.getElementById("title").innerHTML = "Leave your comment for (" + selectedText + ")";
+// document.getElementById("demo1").innerHTML = "The Page URL Is : " +  pageUrl;
+// document.getElementById("demo2").innerHTML = "The Page Title Is: " + pageTitle;
 
 
 // check if is logged in (API Storage)
@@ -15,19 +15,19 @@ chrome.storage.local.get(['loggedin'], function(result)
       console.log('Value currently is ' + result.loggedin);
       if(result.loggedin ==  true)
       {
-          alert("logged")
+        //   alert("logged")
       }
     
       else
       {
-          alert("not logged")
+        //   alert("not logged")
       }
   });
 
 
 document.addEventListener('DOMContentLoaded', function() 
 {
-    var link = document.getElementById('btn');
+    var link = document.getElementById('reply');
     link.addEventListener('click', function() {
         call();
     });
