ebbaff73f712ce26987362a3651ccb5157493d17
Mohammad Smadi <mo.smadi@outlook.com>
2022-04-14 22:44:15 +0300

    solve content security policy by add (addEventListener)

--- public/annotationPage/modalScript.js
+++ public/annotationPage/modalScript.js
@@ -7,3 +7,18 @@ var pageTitle = params.get("pageTitle")
 document.getElementById("demo").innerHTML = "The Selected Word Is : " + selectedText;
 document.getElementById("demo1").innerHTML = "The Page URL Is : " +  pageUrl;
 document.getElementById("demo2").innerHTML = "The Page Title Is: " + pageTitle;
+
+
+document.addEventListener('DOMContentLoaded', function() 
+{
+    var link = document.getElementById('btn');
+    link.addEventListener('click', function() {
+        call();
+    });
+});
+
+
+function call()
+{
+    alert("sds")
+}
