7d06a616a2885568b85496b4adcc0db33f4fe2e9
Mohammad Smadi <mo.smadi@outlook.com>
2022-04-24 12:12:05 +0300

    open starter page when click

--- src/components/Home/Mutual Components/aboutButton.jsx
+++ src/components/Home/Mutual Components/aboutButton.jsx
@@ -1,39 +1,20 @@
-import React, { Component } from "react";
-import Modal from './modal';
-// import Content from "./aboutContent";
+import React from "react";
 
-class About extends Component
+const About = () => 
 {
-  constructor() {
-    super();
-    this.state = {
-      show: false
-    };
-    this.showModal = this.showModal.bind(this);
-    this.hideModal = this.hideModal.bind(this);
-  }
-
-  showModal = () => {
-    this.setState({ show: true });
-  };
-
-  hideModal = () => {
-    this.setState({ show: false });
-  };
-  render() 
+  const openPage = () =>
   {
-    return (
-              <div className="fashion-studio-border pt-2" style={{ marginTop: "10px" }}>
-                <Modal show={this.state.show} handleClose={this.hideModal}>
-          <p>Hello Smadi </p>
-        </Modal>
-              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-              <a href="#">
-                  <span className="fashion-studio" onClick={this.showModal} >About</span>
-              </a>
-              </div>
-    );
+    window.open("http://localhost:3000/#/about");
   }
-};
+
+    return (
+      <div className="fashion-studio-border pt-2" style={{ marginTop: "10px" }}>
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <a href="#">
+          <span className="fashion-studio" onClick={openPage}>About</span>
+        </a>
+      </div>
+    );
+}
 
 export default About;
--- src/components/Home/Mutual Components/pagesButton.jsx
+++ src/components/Home/Mutual Components/pagesButton.jsx
@@ -2,11 +2,16 @@ import React from "react";
 
 const AllPages = () => 
 {
+  const openPage = () =>
+  {
+    window.open("http://localhost:3000/#/table");
+  }
+
     return (
       <div className="wishlist-border pt-2">
         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
         <a href="#">
-          <span className="wishlist">Websites used anottation</span>
+          <span className="wishlist" onClick={openPage}>Websites used anottation</span>
         </a>
       </div>
     );
